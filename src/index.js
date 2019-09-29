064a387bbffdab7df6a4bc013a6b069ea3efebbc
hritikchokker <hchokker7@gmail.com>
2019-09-29 17:15:19 +0530

    switched to class based development

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const App = () =>{
-    return (
-            <h1>From React</h1>
-    )
+
+class App extends React.Component{
+    render() {
+         window.navigator.geolocation.getCurrentPosition(
+             (pos) => console.log(pos, "u r here"),
+             (err) => console.log(err, "something wrong happened")
+         )
+        return <div>Latitude: </div>
+    }
 }
+
 ReactDom.render(<App />, document.querySelector('#root'));
+
+
+
+// const App = () => {
+
+//     return ( <
+//         div > Latitude: < /div>
+//     )
+// }
