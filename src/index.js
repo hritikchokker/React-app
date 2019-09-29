6432ffcbab8190746cab5b2a098d60c837cff3ec
hritikchokker <hchokker7@gmail.com>
2019-09-29 15:17:50 +0530

    first reusable component added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import CommentDetail from './commentDetails';
 const App = () => {
-    const labelText = "Enter Your Name";
-    const buttonText = "click to Submit";
     return (
-        <div>
-            <label htmlFor="name">{labelText}</label>
-            <input id="name" type="text" />
-            <button style={{backgroundColor: 'blue',color: 'white'}} >{buttonText}</button>
+        <div className="ui container comments">
+          <CommentDetail />
         </div>
 
     )
 };
 ReactDom.render(<App />, document.querySelector('#root'));
+
+
+
+// {/* <div>
+//     <label htmlFor="name">{labelText}</label>
+//     <input id="name" type="text" />
+//     <button style={{ backgroundColor: 'blue', color: 'white' }} >{buttonText}</button>
+// </div> */}
