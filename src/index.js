f60414f7d50de3d3e1b4f3141c500563d4b06c5e
hritikchokker <hchokker7@gmail.com>
2019-10-28 17:01:43 +0530

    season complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-useless-constructor */
 import React from 'react';
 import ReactDom from 'react-dom';
-// import SeasonDisplay from './seasonDisplay';
+import SeasonDisplay from './seasonDisplay';
+import Spinner from './spinner';
 
 
 class App extends React.Component {
@@ -10,28 +11,46 @@ class App extends React.Component {
         //* this is the exeption for the rule written below
         //* this is the only time we do direct assignment to this.state
         this.state = {lat: null, errorMessage: ''};
+       
+    }
+    state = { lat: null, errorMessage: '' };
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 console.log(position);
-                this.setState({lat: position.coords.latitude});
+                this.setState({ lat: position.coords.latitude });
                 //* never do this kind of declaration
                 //* this.setState.lat = position.coords.latitude;
             },
-            err =>{
-                this.setState({errorMessage: err.message});
-                 console.log(err)
-                }
+            err => {
+                this.setState({ errorMessage: err.message });
+                console.log(err)
+            }
         );
+        console.log('component was rerendered to the screen');   
     }
+    componentDidUpdate(){
+        console.log('component was updated ~ it rerendered');
+        
+    }
+    renderContent(){
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage} </div>
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+        return <Spinner message="please Accept locaton request" />
+    }
+    
+
     render() {
-     if(this.state.errorMessage && !this.state.lat){
-         return <div> Error: {this.state.errorMessage} </div>
-     }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <div> Latitude: {this.state.lat} </div>
-     }
-     return <div> Loading ...</div>
-    }
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
+        }
 }
 
 ReactDom.render(<App />, document.querySelector('#root'));
